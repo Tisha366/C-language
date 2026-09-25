25809e2c8a5ff703701f095cb549fc661b1c84dd
Tisha366 <tishabachar65@gmail.com>
2026-09-25 16:37:54 +0530

    Add files via upload

---
+++ Tisha 1.cpp
@@ -0,0 +1,14 @@
+//w.a.c.p display odd numbers from 1 to n//
+#include <stdio.h>
+int main()
+{
+	int n,i;
+	printf("Enter n:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i+=2)
+	{
+		printf("%d",i);
+	}
+	return 0;
+}
+
